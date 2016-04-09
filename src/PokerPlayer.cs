bd369c6546293ac1178e308e528099ab7c59d8bb
Щелканов Евгений Олегович <eshchelkanov@kontur>
2016-04-09 14:16:28 +0500

    1

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -18,25 +18,25 @@ namespace Nancy.Simple
 
         public static int isBet(GameState state)
         {
-            var cards = state.GetMyCards();
-            var card1 = cards[0];
-            var card2 = cards[1];
-
-            if (card1.Type == CardType.Ace && card2.Type == CardType.Ace)
-            {
-                return 10;
-            }
-            if (card1.Type == CardType.King && card2.Type == CardType.King)
-            {
-                return 8;
-            }
-            if (card1.Type == CardType.Queen && card2.Type == CardType.Queen)
-            {
-                return 6;
-            }
-
+//            var cards = state.GetMyCards();
+//            var card1 = cards[0];
+//            var card2 = cards[1];
+//
+//            if (card1.Type == CardType.Ace && card2.Type == CardType.Ace)
+//            {
+//                return 10;
+//            }
+//            if (card1.Type == CardType.King && card2.Type == CardType.King)
+//            {
+//                return 8;
+//            }
+//            if (card1.Type == CardType.Queen && card2.Type == CardType.Queen)
+//            {
+//                return 6;
+//            }
+//
+//            return 0;
             return 0;
-
         }
 	}
 }
