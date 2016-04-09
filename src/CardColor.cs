73226db0c26c53acd420d7fc30f53330ba3b6367
disturm <disturm@hotmail.com>
2016-04-09 12:43:23 +0500

    Карты, масти

---
+++ src/CardColor.cs
@@ -0,0 +1,22 @@
+﻿namespace Nancy.Simple
+{
+    public enum CardColor
+    {
+        /// <summary>
+        /// Буби
+        /// </summary>
+        Diamonds,
+        /// <summary>
+        /// Пики
+        /// </summary>
+        Spades,
+        /// <summary>
+        /// Черви
+        /// </summary>
+        Hearts,
+        /// <summary>
+        /// Трефы
+        /// </summary>
+        Clubs
+    }
+}
