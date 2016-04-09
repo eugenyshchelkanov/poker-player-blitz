23b69418dfe7532df7ba8c5b984aab310c8849ba
tim <timofei@skbkontur.ru>
2016-04-09 17:51:38 +0500

    stupid isBet

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -18,24 +18,29 @@ namespace Nancy.Simple
 
         public static int isBet(GameState state)
         {
-//            var cards = state.GetMyCards();
-//            var card1 = cards[0];
-//            var card2 = cards[1];
-//
-//            if (card1.Type == CardType.Ace && card2.Type == CardType.Ace)
-//            {
-//                return 10;
-//            }
-//            if (card1.Type == CardType.King && card2.Type == CardType.King)
-//            {
-//                return 8;
-//            }
-//            if (card1.Type == CardType.Queen && card2.Type == CardType.Queen)
-//            {
-//                return 6;
-//            }
-//
-//            return 0;
+            var cards = state.GetMyCards();
+            var card1 = cards[0];
+            var card2 = cards[1];
+
+            if (card1.Rank == "A" && card2.Rank == "A")
+            {
+                return 10;
+            }
+            if (card1.Rank == "K" && card2.Rank == "K")
+            {
+                return 8;
+            }
+            if (card1.Rank == "Q" && card2.Rank == "Q")
+            {
+                return 6;
+            }
+
+            if (card1.Rank.Equals(card2.Rank))
+            {
+                return 5;
+            }
+
+
             return 0;
         }
 	}
