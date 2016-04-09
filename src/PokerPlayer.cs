5b6b4f8625639271087cb52e8b7ecfc20e3b4be1
tim <timofei@skbkontur.ru>
2016-04-09 17:18:25 +0500

    1

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -15,6 +15,29 @@ namespace Nancy.Simple
 		{
 			//TODO: Use this method to showdown
 		}
+
+        public static int isBet(GameState state)
+        {
+            var cards = state.GetMyCards();
+            var card1 = cards[0];
+            var card2 = cards[1];
+
+            if (card1.Type == CardType.Ace && card2.Type == CardType.Ace)
+            {
+                return 10;
+            }
+            if (card1.Type == CardType.King && card2.Type == CardType.King)
+            {
+                return 8;
+            }
+            if (card1.Type == CardType.Queen && card2.Type == CardType.Queen)
+            {
+                return 6;
+            }
+
+            return 0;
+
+        }
 	}
 }
 
