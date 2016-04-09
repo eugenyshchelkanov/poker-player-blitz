035177d05573fb187ee14af0b26b734335286900
disturm <disturm@hotmail.com>
2016-04-09 13:53:01 +0500

    Начало комбинаций

---
+++ src/Combinator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple.Comb
+{
+    public class Combination
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+        public Combination(string name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+    }
+
+    public class CombinationWithHighCard
+    {
+        public Combination Combination { get; set; }
+        public int Rank { get; set; }
+        public Card[] Cards { get; set; }
+
+        public CombinationWithHighCard(Combination combination, int rank, Card[] cards)
+        {
+            Combination = combination;
+            Rank = rank;
+            Cards = cards;
+        }
+    }
+
+    public static class Combinations
+    {
+    }
+
+    public class Combinator
+    {
+        public CombinationWithHighCard Combinate(Card[] cards)
+        {
+            return null;
+        }
+    }
+}
