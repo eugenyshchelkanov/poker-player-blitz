e435477572c150c9a656c2e18eee42760609acad
Щелканов Евгений Олегович <eshchelkanov@kontur>
2016-04-09 15:38:58 +0500

    1

--- src/Logic/Flop.cs
+++ src/Logic/Flop.cs
@@ -10,35 +10,35 @@
 
             if (card1.Rank == "A" && card2.Rank == "A")
             {
-                return new LogicResult() { BestBetInPots = 10, CanRespondToAllIn = true };
+                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
             }
             if (card1.Rank == "K" && card2.Rank == "K")
             {
-                return new LogicResult() { BestBetInPots = 8, CanRespondToAllIn = true };
+                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
             }
             if (card1.Rank == "Q" && card2.Rank == "Q")
             {
-                return new LogicResult() { BestBetInPots = 6, CanRespondToAllIn = true };
+                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
             }
 
             if (card1.Rank.Equals(card2.Rank))
             {
-                return new LogicResult() { BestBetInPots = 5, CanRespondToAllIn = false };
+                return new LogicResult() { BestBetInPots = 0.5, CanRespondToAllIn = false };
             }
 
             if (card1.Suit.Equals(card2.Suit))
             {
-                return new LogicResult() { BestBetInPots = 5, CanRespondToAllIn = false };
+                return new LogicResult() { BestBetInPots = 0.25, CanRespondToAllIn = false };
             }
 
             if (card1.Rank == "A" || card1.Rank == "K" || card1.Rank == "Q" || card2.Rank == "A" || card2.Rank == "K" || card2.Rank == "Q")
             {
-                return new LogicResult() { BestBetInPots = 5, CanRespondToAllIn = false };
+                return new LogicResult() { BestBetInPots = 0.25, CanRespondToAllIn = false };
             }
 
             if (card1.Suit.Equals(card2.Suit))
             {
-                return new LogicResult() { BestBetInPots = 5, CanRespondToAllIn = false };
+                return new LogicResult() { BestBetInPots = 0.25, CanRespondToAllIn = false };
             }
 
             return new LogicResult() { BestBetInPots = 0, CanRespondToAllIn = false };
