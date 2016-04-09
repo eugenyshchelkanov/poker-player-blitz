929e5b5a9874ab45b70b85a4e74e28b5daafb3af
Щелканов Евгений Олегович <eshchelkanov@kontur>
2016-04-09 15:46:00 +0500

    1

--- src/Logic/Turn.cs
+++ src/Logic/Turn.cs
@@ -4,52 +4,7 @@
     {
         public static LogicResult Result(GameState state)
         {
-            var cards = state.GetMyCards();
-            var card1 = cards[0];
-            var card2 = cards[1];
-
-            if (card1.Rank == "A" && card2.Rank == "A")
-            {
-                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
-            }
-            if (card1.Rank == "A" && card2.Rank == "K" && card1.Suit == card2.Suit)
-            {
-                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
-            }
-            if (card1.Rank == "K" && card2.Rank == "K")
-            {
-                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
-            }
-            if (card1.Rank == "Q" && card2.Rank == "Q")
-            {
-                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
-            }
-            if (card1.Rank == "J" && card2.Rank == "J")
-            {
-                return new LogicResult() { BestBetInPots = 1, CanRespondToAllIn = true };
-            }
-
-            if (card1.Rank.Equals(card2.Rank))
-            {
-                return new LogicResult() { BestBetInPots = 0.5, CanRespondToAllIn = false };
-            }
-
-            if (card1.Suit.Equals(card2.Suit))
-            {
-                return new LogicResult() { BestBetInPots = 0.25, CanRespondToAllIn = false };
-            }
-
-            if (card1.Rank == "A" || card1.Rank == "K" || card1.Rank == "Q" || card2.Rank == "A" || card2.Rank == "K" || card2.Rank == "Q")
-            {
-                return new LogicResult() { BestBetInPots = 0.25, CanRespondToAllIn = false };
-            }
-
-            if (card1.Suit.Equals(card2.Suit))
-            {
-                return new LogicResult() { BestBetInPots = 0.25, CanRespondToAllIn = false };
-            }
-
-            return new LogicResult() { BestBetInPots = 0, CanRespondToAllIn = false };
+            return Preflop.Result(state);
         }
     }
 }
